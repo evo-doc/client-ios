d77e7350bd069975699a12d135d0114fa74342e8
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2018-12-26 20:58:07 +0100

    Initial Commit

---
+++ bi-ios-evodoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bi-ios-evodoc
+//
+//  Created by Sergey Dunaevskiy on 26/12/2018.
+//  Copyright © 2018 evodoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+
+}
+
