cdba80e6905b0eec8d7b9cd713d03ba421375181
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2019-02-05 20:16:43 +0100

    feature: packages page

--- evodoc/Controller/PackagesController.swift
+++ evodoc/Controller/PackagesController.swift
@@ -17,8 +17,17 @@ class PackagesController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
         self.title = "Packages";
+        
+        let packagesView = PackagesView();
+        view.addSubview(packagesView);
+        packagesView.snp.makeConstraints{
+            make in
+            make.left.equalTo(view.snp.leftMargin)
+            make.right.equalTo(view.snp.rightMargin)
+            make.top.equalTo(view.snp.top)
+            make.bottom.equalTo(view.snp.bottom)
+        }
     }
     
     
