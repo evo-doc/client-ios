67f219d0eb1e5c02eb0d94de002029e57377416e
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2019-02-09 21:14:04 +0100

    feture: project create and update API methods

---
+++ evodoc/Model/Projects/ProjectAPICreateModel.swift
@@ -0,0 +1,11 @@
+//
+//  ProjectAPICreateModel.swift
+//  EvoDoc
+//
+//  Created by Sergey Dunaevskiy on 09/02/2019.
+//  Copyright © 2019 evodoc. All rights reserved.
+//
+
+struct ProjectAPICreateModel: Codable {
+    var id: Int;
+}
--- evodoc/Tools/API/ProjectAPI.swift
+++ evodoc/Tools/API/ProjectAPI.swift
@@ -66,5 +66,110 @@ class ProjectAPI {
                 }
             }
     }
-}
+    
+    /**
+     Create a new project for the current user.
+     
+     # Example call
+     
+     ```
+     ProjectAPI.createProject(
+         name: "Name",
+         description: "Description",
+         contributors: [],
+         callback: { project in
+            print(project.id)
+         }
+     )
+     ```
+    */
+    static func createProject(
+        name: String,
+        description: String,
+        contributors: [String],
+        callback: @escaping ((ProjectAPICreateModel) -> ())
+        ){
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer " + (UserDefaults.standard.value(forKey: "token") as? String ?? "")
+        ]
+        
+        Alamofire.request(
+            ServerConfig.host("/projects/project"),
+            method: .post,
+            parameters: [
+                "name": name,
+                "description": description,
+                "collaborators": [
+                    "contributors": contributors
+                ]
+            ],
+            encoding: JSONEncoding.default,
+            headers: headers
+            ).responseJSON { response in
+                if let data = response.data {
+                    do {
+                        let jsonDecoder = JSONDecoder()
+                        let project = try
+                            jsonDecoder.decode(ProjectAPICreateModel.self, from: data);
+                        callback(project)
+                    } catch {
+                        print(error)
+                        Utilities.viewAlert(title: "Cannot create", message: "The name of the project is too short.")
+                    }
+                }
+        }
+    }
+    
 
+    /**
+     Update an existing project
+     
+     # Example call
+     
+     ```
+     ProjectAPI.patchProject(
+         id: 33,
+         name: "New name",
+         description: "New description",
+         callback: { success in
+             if (success) {
+                print("Good")
+             }
+         }
+     )
+     ```
+     */
+    static func patchProject(
+        id: Int,
+        name: String,
+        description: String,
+        callback: @escaping ((Bool) -> ())
+        ){
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer " + (UserDefaults.standard.value(forKey: "token") as? String ?? "")
+        ]
+        
+        Alamofire.request(
+            ServerConfig.host("/projects/\(id)"),
+            method: .patch,
+            parameters: [
+                "name": name,
+                "description": description,
+                ],
+            encoding: JSONEncoding.default,
+            headers: headers
+            )
+            .validate(statusCode: 200...200)
+            .responseData { response in
+                switch response.result {
+                case .success:
+                    callback(true)
+                case .failure:
+                    Utilities.viewAlert(title: "Cannot update", message: "The name of the project is too short.")
+                    callback(false)
+                }
+        }
+    }
+}
